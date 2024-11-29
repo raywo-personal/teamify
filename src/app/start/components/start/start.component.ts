60290ffbeae1f6d3bdef2622d06e6c6f4b4b5760
Ray Wojciechowski <ray@skillbird.de>
2024-11-29 08:49:26 +0100

    Fix multiple add behaviour in person edit
    Cleanup start component

--- src/app/start/components/start/start.component.ts
+++ src/app/start/components/start/start.component.ts
@@ -1,26 +1,12 @@
 import {Component} from '@angular/core';
-import {createPerson, Person} from '../../../persons/models/person.model';
-import {PersonEditComponent} from '../../../persons/components/person-edit/person-edit.component';
-import {AddHeaderBarComponent} from '../../../shared/components/add-header-bar/add-header-bar.component';
-import {PersonViewComponent} from '../../../persons/components/person-view/person-view.component';
 
 
 @Component({
   selector: 'app-start',
-  imports: [
-    PersonEditComponent,
-    AddHeaderBarComponent,
-    PersonViewComponent
-  ],
+  imports: [],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
 })
 export class StartComponent {
 
-  protected person = createPerson("");
-
-
-  onSaved(person: Person) {
-    this.person = person;
-  }
 }

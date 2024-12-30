9306557d53f59c9be3ae6a9178661a5a14ea4ae4
Ray Wojciechowski <ray@skillbird.de>
2024-12-30 13:22:41 +0100

    Refactor start component and add steps and teams overview
    Introduce `StepsComponent` and `TeamsOverviewComponent` to modularize
    functionality. Adjust styles and updated templates for a cohesive
    layout.

--- src/app/start/components/start/start.component.ts
+++ src/app/start/components/start/start.component.ts
@@ -1,48 +1,18 @@
-import {Component, inject} from '@angular/core';
-import {StepComponent} from '../step/step.component';
-import {TimeSlotService} from '../../../timeslots/services/time-slot.service';
-import {PriorKnowledgeService} from '../../../prior-knowledge/services/prior-knowledge.service';
-import {PersonService} from '../../../persons/services/person.service';
-import {TeamService} from '../../../teams/services/team.service';
+import {Component} from '@angular/core';
+import {TeamsOverviewComponent} from '../teams-overview/teams-overview.component';
+import {StepsComponent} from '../steps/steps.component';
 
 
 @Component({
   selector: 'app-start',
   imports: [
-    StepComponent
+    TeamsOverviewComponent,
+    StepsComponent
   ],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
 })
 export class StartComponent {
 
-  private timeSlotService = inject(TimeSlotService);
-  private knowledgeService = inject(PriorKnowledgeService);
-  private personService = inject(PersonService);
-  private teamService = inject(TeamService);
-
-  protected step1IsDone = false;
-  protected step2IsDone = false;
-  protected step3IsDone = false;
-  protected step4IsDone = false;
-
-
-  constructor() {
-    this.timeSlotService.slotCount$.subscribe(count => {
-      this.step1IsDone = count > 0;
-    });
-
-    this.knowledgeService.knowledgeList$.subscribe(list => {
-      this.step2IsDone = list.length > 0;
-    });
-
-    this.personService.personCount$.subscribe(count => {
-      this.step3IsDone = count > 0;
-    });
-
-    this.teamService.teams$.subscribe(teams => {
-      this.step4IsDone = teams.length > 0;
-    });
-  }
 
 }

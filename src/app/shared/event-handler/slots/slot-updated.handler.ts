eca8f87e391332d68819bbb2122f12cf9442d7b6
Ray Wojciechowski <ray@skillbird.de>
2025-01-02 17:26:52 +0100

    Refactor grouping layout
    Update layout and styling of grouping and person-view components to
    implement stacked view.

--- src/app/shared/event-handler/slots/slot-updated.handler.ts
+++ src/app/shared/event-handler/slots/slot-updated.handler.ts
@@ -3,6 +3,7 @@ import {TimeSlot} from '../../../timeslots/models/time-slot.model';
 import {EventType} from '../../event-bus/event.model';
 import {inject, Injectable} from '@angular/core';
 import {TeamService} from '../../../teams/services/team.service';
+import {PersonService} from '../../../persons/services/person.service';
 
 
 @Injectable({
@@ -11,11 +12,18 @@ import {TeamService} from '../../../teams/services/team.service';
 export class SlotUpdatedHandler implements EventHandler<TimeSlot> {
 
   private teamService = inject(TeamService);
+  private personService = inject(PersonService);
+
   public readonly eventType: EventType = EventType.SLOT_UPDATED;
 
 
   public handle(slot: TimeSlot) {
     this.teamService.updateTeamForSlot(slot);
+    this.personService.persons.forEach(person => {
+      person.timeSlots
+        .filter(ts => ts.timeSlot.id === slot.id)
+        .forEach(ts => ts.timeSlot = slot);
+    });
   }
 
 }

07857ecde94be641e4436b73f69b73832015ebfe
Ray Wojciechowski <ray@skillbird.de>
2025-01-11 18:10:18 +0100

    Add team-specific person export functionality
    Implement the ability to export person data for individual teams.
    Update the UI with a dropdown for selecting teams and add a new method
    in `PersistenceService` to generate export URLs for team-specific
    person data.

--- src/app/settings/components/export/export.component.ts
+++ src/app/settings/components/export/export.component.ts
@@ -1,16 +1,29 @@
 import {Component, inject} from '@angular/core';
 import {PersistenceService} from '../../../shared/services/persistence.service';
+import {NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle} from '@ng-bootstrap/ng-bootstrap';
+import {TeamService} from '../../../teams/services/team.service';
+import {AsyncPipe} from '@angular/common';
+import {Team} from '../../../teams/models/team.model';
 
 
 @Component({
   selector: 'app-export',
-  imports: [],
+  imports: [
+    NgbDropdown,
+    NgbDropdownToggle,
+    NgbDropdownMenu,
+    NgbDropdownItem,
+    AsyncPipe
+  ],
   templateUrl: './export.component.html',
   styleUrl: './export.component.scss'
 })
 export class ExportComponent {
 
   private persistenceService = inject(PersistenceService);
+  private teamService = inject(TeamService);
+
+  protected teams$ = this.teamService.teams$;
 
 
   protected exportAllData() {
@@ -34,6 +47,17 @@ export class ExportComponent {
   protected exportAllPersonData() {
     const url = this.persistenceService.personsExportURL();
     this.startDownload(url, "all-persons.json");
+  }
+
+
+  protected exportTeamPersonData(team: Team) {
+    const url = this.persistenceService.personsOfTeamExportURL(team);
+    this.startDownload(url, `${this.nameForTeam(team)}-persons.json`);
+  }
+
+
+  protected nameForTeam(team: Team) {
+    return team.name || team.timeSlot.description;
   }
 
 
